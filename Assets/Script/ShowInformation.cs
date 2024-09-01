93af48aebff0830c2de0b4205282339cb50570ad
userName <userEmail>
2024-09-01 14:36:27 +0800

    場景二 VR視角 以及UI射線設置
    第二個場景 功能都調整為能在vr環境下使用

--- Assets/Script/ShowInformation.cs
+++ Assets/Script/ShowInformation.cs
@@ -5,23 +5,22 @@ using UnityEngine.UI;
 
 public class ShowInformation : MonoBehaviour
 {
-    public Button footbook;
-    public Button instragrum;
-    public Button xwitter;
-    public Canvas footbook_C;
-    public Canvas instragrum_C;
-    public Canvas xwitter_C;
-    // Start is called before the first frame update
+    public Button footbook;//footbook按鈕
+    public Button instragrum; //instragrum按鈕
+    public Button xwitter;// xwitter按鈕
+    public Canvas footbook_C; //footbook的canva
+    public Canvas instragrum_C; //instragrum的canva
+    public Canvas xwitter_C; //xwitter的canva
     void Start()
     {
-        footbook_C.gameObject.SetActive(false);
+        footbook_C.gameObject.SetActive(false); //開始時把所有圖片隱藏
         instragrum_C.gameObject.SetActive(false);
         xwitter_C.gameObject.SetActive(false);
-        footbook.onClick.AddListener(OnFootbookClick);
+        footbook.onClick.AddListener(OnFootbookClick); //在每一個按鈕上加上一個監聽器
         instragrum.onClick.AddListener(OninstragrumClick);
         xwitter.onClick.AddListener(OnxwitterClick);
     }
-    void OnFootbookClick()
+    void OnFootbookClick()　//在按鈕按下時把其他的canva隱藏
     {
         footbook_C.gameObject.SetActive(true);
         instragrum_C.gameObject.SetActive(false);
